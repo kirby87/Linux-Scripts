1dde908281c39bc7e91747b34d3c0c6e4cf77c08
kirby87 <19243687+kirby87@users.noreply.github.com>
2018-11-01 13:16:48 -0600

    Update Get-ADGroup Users.ps1
    USE AT YOUR OWN RISK!!!

--- powershell/active directory/Get-ADGroup Users.ps1
+++ powershell/active directory/Get-ADGroup Users.ps1
@@ -1,1 +1,33 @@
-###
+cls
+
+$scope = "Global"
+
+Function Get-GroupMember{
+    Param(
+        [parameter(Mandatory=$true)]
+        [string]
+        $scope
+    )
+$Groups = Get-ADGroup -Filter {
+    GroupScope -eq $scope -and Members -ne "NULL"
+} -Properties Name | Select-Object Name, @{
+    Name="GroupMembers";Expression={
+        (Get-ADGroupMember -Identity "$_" | 
+            Select-Object -ExpandProperty SamAccountName) -join "`n"
+    }
+}
+}
+$Groups | Format-Table -AutoSize -Wrap
+
+Function Get-GroupUsers{
+$Users = foreach( $Group in $Groups ){
+    Get-ADGroupMember -Identity $Group | foreach {
+            [PSCustomObject]@{
+            Group = $Group.Name
+            UserName = $_.SamAccountName
+            }
+        }
+    }
+ }
+$Users | Format-Table -AutoSize -Wrap
+#$Users | Out-GridView
